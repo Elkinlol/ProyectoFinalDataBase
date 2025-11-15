d58cf0c5957c38d772ed2e39b7b1dbda43fc7b81
ElkinBG <elkinlol13@gmail.com>
2025-11-15 17:00:20 -0500

    add dtos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module co.bases.datos.proyectofinaldatabase {
     requires javafx.controls;
     requires javafx.fxml;
+    requires static lombok;
 
 
     opens co.bases.datos.proyectofinaldatabase to javafx.fxml;
