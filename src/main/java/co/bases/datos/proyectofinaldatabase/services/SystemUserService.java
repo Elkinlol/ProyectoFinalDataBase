77aef79557195785d62136baac324c8e4abaf1b9
ElkinBG <elkinlol13@gmail.com>
2025-11-15 17:46:26 -0500

    add SystemUsersService

---
+++ src/main/java/co/bases/datos/proyectofinaldatabase/services/SystemUserService.java
@@ -0,0 +1,55 @@
+package co.bases.datos.proyectofinaldatabase.services;
+
+import co.bases.datos.proyectofinaldatabase.dtos.LoginDTO;
+import co.bases.datos.proyectofinaldatabase.dtos.RegisterDTO;
+import co.bases.datos.proyectofinaldatabase.dtos.UserDTO;
+import co.bases.datos.proyectofinaldatabase.model.SystemUser;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+
+public class SystemUserService {
+
+    private final UserDAO userDAO = UserDAO();
+
+    public UserDTO login (LoginDTO loginDTO)throws Exception{
+
+        if(loginDTO.userName() == null || loginDTO.userName().isEmpty()){
+            throw new Exception("El usuario no puede estar vacio");
+        }
+        if(loginDTO.password()==null || loginDTO.password().isBlank()){
+            throw new Exception("El usuario no puede estar vacio");
+        }
+        SystemUser user = userDAO.findByUserName(loginDTO.userName());
+        if(user==null){
+            throw new Exception("El usuario no existe");
+        }
+        if(!user.getPassword().equals(loginDTO.password())){
+        }
+        return new UserDTO(
+                user.getCedula(),
+                user.getUserName(),
+                user.getFullName(),
+                user.getRole()
+        );
+    }
+
+    public UserDTO register (RegisterDTO dto) throws Exception{
+        if(userExistente!=null){
+            throw new Exception("El nombre de usuario ya existe");
+        }
+        SystemUser nuevo =  SystemUser.builder()
+                .cedula(dto.cedula())
+                .userName(dto.userName())
+                .password(dto.password())
+                .fullName(dto.fullName())
+                .role(dto.role())
+                .build();
+        return new UserDTO(nuevo.getCedula(), nuevo.getUserName(), nuevo.getFullName()
+                ,nuevo.getRole());
+    }
